1dd164b9c24e7247933845ec1f3e657e0769c6e0
Henko Rabie <henko.rabie@media24.com>
2021-01-11 12:35:31 +0200

    Adding jsonpropertyname to filx casing issue

--- WordStack.Web/Models/Word.cs
+++ WordStack.Web/Models/Word.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace WordStack.Web.Models
 {
     public class Word
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("stringValue")]
         public string StringValue { get; set; }
 
+        [JsonPropertyName("wordTypeId")]
         public int WordTypeId { get; set; }
         public WordType WordType { get; set; }
     }
--- WordStack.Web/Models/WordType.cs
+++ WordStack.Web/Models/WordType.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace WordStack.Web.Models
 {
     public class WordType
     {
+        [JsonPropertyName("id")]
         public int Id { get; set; }
+        [JsonPropertyName("stringValue")]
         public string StringValue { get; set; }
     }
 }
